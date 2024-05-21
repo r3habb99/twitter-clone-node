d420e1e8aaa6ec63a1f8004bf056e1b7c3faf039
Rishabh Prajapati <rishabh.prajapati@brainvire.com>
2024-05-21 14:35:37 +0530

    Added new features

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const User = require('../schemas/UserSchema');
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
@@ -12,17 +14,42 @@ router.get('/', (req, res, next) => {
   res.status(200).render('register');
 });
 
-router.post('/', (req, res, next) => {
-  let firstName = req.body.firstName.trim();
-  let lastName = req.body.lastName.trim();
-  let username = req.body.username.trim();
-  let email = req.body.email.trim();
-  let password = req.body.password;
+router.post('/', async (req, res, next) => {
+  var firstName = req.body.firstName.trim();
+  var lastName = req.body.lastName.trim();
+  var username = req.body.username.trim();
+  var email = req.body.email.trim();
+  var password = req.body.password;
 
-  let payload = req.body;
+  var payload = req.body;
 
   if (firstName && lastName && username && email && password) {
-    console.log('String');
+    var user = await User.findOne({
+      $or: [{ username: username }, { email: email }],
+    }).catch((error) => {
+      console.log(error);
+      payload.errorMessage = 'Something went wrong.';
+      res.status(200).render('register', payload);
+    });
+
+    if (user == null) {
+      // No user found
+      let data = req.body;
+      data.password = await bcrypt.hash(password, 10);
+
+      User.create(data).then((user) => {
+        req.session.user = user;
+        return res.redirect('/');
+      });
+    } else {
+      // User found
+      if (email == user.email) {
+        payload.errorMessage = 'Email already in use.';
+      } else {
+        payload.errorMessage = 'Username already in use.';
+      }
+      res.status(200).render('register', payload);
+    }
   } else {
     payload.errorMessage = 'Make sure each field has a valid value.';
     res.status(200).render('register', payload);
--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const authMiddleware = require('./middlewares/authMiddleware');
+const middleware = require('./middlewares/authMiddleware');
 const path = require('path');
 const bodyParser = require('body-parser');
+const mongoose = require('./database');
+const session = require('express-session');
 
-const server = app.listen(port, () => {
-  const protocol = 'http';
-  const host = 'localhost';
-  console.log(`Server is up and running at ${protocol}://${host}:${port}/`);
-});
+const server = app.listen(port, () =>
+  console.log('Server listening on port ' + port)
+);
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+  session({
+    secret: 'bbq chips',
+    resave: true,
+    saveUninitialized: false,
+  })
+);
 
 // Routes
 const loginRoute = require('./routes/loginRoutes');
@@ -24,9 +32,10 @@ const registerRoute = require('./routes/registerRoutes');
 app.use('/login', loginRoute);
 app.use('/register', registerRoute);
 
-app.get('/', authMiddleware.requireLogin, (req, res, next) => {
+app.get('/', middleware.requireLogin, (req, res, next) => {
   let payload = {
     pageTitle: 'Home',
+    userLoggedIn: req.session.user,
   };
 
   res.status(200).render('home', payload);
