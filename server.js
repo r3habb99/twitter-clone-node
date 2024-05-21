e27cf3e844da4c759153ec83694304ed1abd9a6c
Rishabh Prajapati <rishabh.prajapati@brainvire.com>
2024-05-21 11:25:10 +0530

    added pug file for frontend

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+app.set('view engine', 'pug');
+app.set('views', 'src/views');
+
+app.get('/', (req, res) => {
+  let payload = {
+    pageTitle: 'Home',
+  };
+
+  res.status(200).render('home', payload);
+});
+
+
+
 const server = app.listen(port, () => {
   const protocol = 'http';
   const host = 'localhost';
