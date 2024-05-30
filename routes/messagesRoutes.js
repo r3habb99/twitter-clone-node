cd1c8b90a3fe1dbf908589bc928d40bbc6473089
Rishabh Prajapati <rishabh.prajapati@brainvire.com>
2024-05-30 18:45:48 +0530

    added chat page

--- routes/messagesRoutes.js
+++ routes/messagesRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+const Chat = require('../schemas/ChatSchema');
 
 router.get('/', (req, res, next) => {
   let payload = {
@@ -18,6 +19,27 @@ router.get('/new', (req, res, next) => {
     userLoggedInJs: JSON.stringify(req.session.user),
   };
   res.status(200).render('newMessage', payload);
+});
+
+router.get('/:chatId', async (req, res, next) => {
+  let userId = req.session.user._id;
+  let chatId = req.params.chatId;
+
+  let chat = await Chat.findOne({
+    _id: chatId,
+    users: { $elemMatch: { $eq: userId } },
+  }).populate('users');
+
+  if (chat == null) {
+    // Check if chat id is really user id
+  }
+
+  res.status(200).render('chatPage', {
+    pageTitle: 'Chat',
+    userLoggedIn: req.session.user,
+    userLoggedInJs: JSON.stringify(req.session.user),
+    chat: chat,
+  });
 });
 
 module.exports = router;
