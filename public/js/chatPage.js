41784bb691f90a035340c9f2784ff0a3008f3f34
Rishabh Prajapati <rishabh.prajapati@brainvire.com>
2024-06-03 16:02:31 +0530

    Added message styling and function

--- public/js/chatPage.js
+++ public/js/chatPage.js
@@ -45,7 +45,37 @@ function sendMessage(content) {
     '/api/messages',
     { content: content, chatId: chatId },
     (data, status, xhr) => {
-      console.log(data);
+      if (xhr.status != 201) {
+        alert('Could not send message');
+        $('.inputTextbox').val(content);
+        return;
+      }
+
+      addChatMessageHtml(data);
     }
   );
+}
+
+function addChatMessageHtml(message) {
+  if (!message?._id) {
+    alert('Message is not valid');
+    return;
+  }
+
+  let messageDiv = createMessageHtml(message);
+
+  $('.chatMessages').append(messageDiv);
+}
+
+function createMessageHtml(message) {
+  let isMine = message.sender._id === userLoggedIn._id;
+  let liClassName = isMine ? 'mine' : 'theirs';
+
+  return `<li class='message ${liClassName}'>
+                <div class='messageContainer'>
+                    <span class='messageBody'>
+                        ${message.content}
+                    </span>
+                </div>
+            </li>`;
 }
