b27a008c7ce4939f8928463eff5a5004268ae03c
Rishabh <rishabh.prajapati@rapidops.com>
2025-03-26 12:49:27 +0530

    removed commented code

--- src/db/database.js
+++ src/db/database.js
@@ -1,27 +1,3 @@
-// const mongoose = require('mongoose');
-// require('dotenv').config();
-// // mongoose.set('useNewUrlParser', true);
-// // mongoose.set('useUnifiedTopology', true);
-// // mongoose.set('useFindAndModify', false);
-
-// const DATABASE_URL = process.env.URL;
-// class Database {
-
-//   connect() {
-//     mongoose
-//       .connect(DATABASE_URL)
-
-//       .then(() => {
-//         console.log('Database connection successful...');
-//       })
-//       .catch((err) => {
-//         console.log('Database connection error ' + err);
-//       });
-//   }
-// }
-
-// module.exports = new Database();
-
 const mongoose = require("mongoose");
 const { logger } = require("../utils/logger");
 const dotenv = require("dotenv");
